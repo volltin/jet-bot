cb2d9f9cc3d1cf9e115fe78b07df1273927269ca
Jiyan He <volltin@live.com>
2023-10-12 15:50:59 +0800

    feat: multi chat tabs and args

--- jet/web.py
+++ jet/web.py
@@ -11,27 +11,50 @@ sys.path.append(str(Path(__file__).parent))
 dotenv.load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
-BOT_NAME = "Jet"
-chat_tab = tabs.create_chat_tab(tab_id="chattab-1")
-writing_tab = tabs.create_writing_tab(tab_id="writingtab")
-speech_tab = tabs.create_speech_tab(tab_id="speechtab")
-demo = gr.TabbedInterface(
-    [chat_tab, writing_tab, speech_tab],
-    tab_names=["Chat", "Writing", "Speech"],
-    css="footer {visibility: hidden}",
-    title=f"Chat with {BOT_NAME} (HJY AI bot)",
-    theme=gr.themes.Soft(),
-)
-
 
 @click.command()
+@click.option(
+    "--bot-name",
+    default="Jet",
+    help="The name of the bot.",
+)
+@click.option(
+    "--num-chat-tabs",
+    default=5,
+    help="The number of chat tabs.",
+)
 @click.option(
     "--share/--no-share",
     default=False,
     help="Share on Gradio's public domain. Share links expire after 72 hours.",
 )
-def main(share):
-    demo.queue(concurrency_count=4).launch(share=share, auth=[("test", "test")])
+@click.option(
+    "--auth-username",
+    default="test",
+    help="Username for basic auth.",
+)
+@click.option(
+    "--auth-password",
+    default="test",
+    help="Password for basic auth.",
+)
+def main(bot_name, num_chat_tabs, auth_username, auth_password, share):
+    chat_tabs = [
+        tabs.create_chat_tab(tab_id=f"chattab{i+1}") for i in range(num_chat_tabs)
+    ]
+    chat_tab_names = [f"Chat {i+1}" for i in range(5)]
+    writing_tab = tabs.create_writing_tab(tab_id="writingtab")
+    speech_tab = tabs.create_speech_tab(tab_id="speechtab")
+    demo = gr.TabbedInterface(
+        [*chat_tabs, writing_tab, speech_tab],
+        tab_names=[*chat_tab_names, "Writing", "Speech"],
+        css="footer {visibility: hidden}",
+        title=f"Chat with {bot_name} (HJY AI bot)",
+        theme=gr.themes.Soft(),
+    )
+    demo.queue(concurrency_count=4).launch(
+        share=share, auth=[(auth_username, auth_password)]
+    )
 
 
 if __name__ == "__main__":
